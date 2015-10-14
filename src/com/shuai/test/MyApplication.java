21d229a120dc6af996a5365eeb9a01dbbd6120bf
jinshiyi11 <49662990@qq.com>
2015-10-14 18:44:42 +0800

    1.添加android:clipChildren属性测试

--- src/com/shuai/test/MyApplication.java
+++ src/com/shuai/test/MyApplication.java
@@ -3,7 +3,7 @@ package com.shuai.test;
 import android.app.Application;
 import android.content.Intent;
 
-import com.shuai.test.tools.WindowChangeDetectingService;
+import com.shuai.test.tools.TopActivityMonitorService;
 
 public class MyApplication extends Application {
 
@@ -11,16 +11,15 @@ public class MyApplication extends Application {
 	public void onCreate() {
 		super.onCreate();
 		
-//		Intent intent=new Intent(this, TopActivityMonitorService.class);
-//		startService(intent);
-		
-		Intent intent0 = new Intent(android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS);
-		intent0.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		startActivity(intent0);
-		
-		
-		Intent intent=new Intent(this, WindowChangeDetectingService.class);
+		Intent intent=new Intent(this, TopActivityMonitorService.class);
 		startService(intent);
+		
+//		Intent intent0 = new Intent(android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS);
+//		intent0.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//		startActivity(intent0);
+		
+		
+
 	}
 
 }

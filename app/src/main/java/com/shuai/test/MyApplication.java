901a1576625879d4046edfcf24f2085ad90fd995
帅维灿 <shuaiweican@douyu.tv>
2018-03-20 17:17:22 +0800

    1.test rxjava

--- app/src/main/java/com/shuai/test/MyApplication.java
+++ app/src/main/java/com/shuai/test/MyApplication.java
@@ -11,7 +11,6 @@ public class MyApplication extends Application {
 
 	public MyApplication() {
 		super();
-        Debug.waitForDebugger();
 	}
 
 	@Override
---
+++ app/src/main/java/com/shuai/test/animation/TestAnimationActivity.java
@@ -0,0 +1,15 @@
+package com.shuai.test.animation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.shuai.test.R;
+
+public class TestAnimationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_animation);
+    }
+}

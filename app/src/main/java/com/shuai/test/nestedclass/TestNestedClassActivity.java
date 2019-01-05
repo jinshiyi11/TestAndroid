8b620e975ef10145b0487ebdd3247ea6866ad8eb
jinshiyi11 <49662990@qq.com>
2019-01-05 21:07:10 +0800

    1.更新版本，添加kotlin支持

---
+++ app/src/main/java/com/shuai/test/nestedclass/TestNestedClassActivity.java
@@ -0,0 +1,44 @@
+package com.shuai.test.nestedclass;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.shuai.test.R;
+
+public class TestNestedClassActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = TestNestedClassActivity.class.getSimpleName();
+    private Button mBtnTest;
+    private String mText = "test";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_nested_class);
+
+
+
+        mBtnTest = (Button) findViewById(R.id.btn_test);
+        mBtnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG,mText);
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.btn_test:
+                break;
+        }
+
+    }
+}

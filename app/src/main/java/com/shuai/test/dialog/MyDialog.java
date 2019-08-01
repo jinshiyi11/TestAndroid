60224d375b27e849b4617b7f3502226d6da99262
jinshiyi11 <49662990@qq.com>
2019-08-02 00:30:45 +0800

    1.测试对话框反复show和dismiss

--- app/src/main/java/com/shuai/test/dialog/MyDialog.java
+++ app/src/main/java/com/shuai/test/dialog/MyDialog.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.Gravity;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.TextView;
 
@@ -37,6 +38,8 @@ public class MyDialog extends Dialog implements DialogInterface.OnDismissListene
 //        setContentView(new FrameLayout(mContext));
 
         WindowManager.LayoutParams attributes = getWindow().getAttributes();
+        attributes.width= ViewGroup.LayoutParams.MATCH_PARENT;
+        attributes.height = 400;
         attributes.gravity= Gravity.BOTTOM;
         getWindow().setAttributes(attributes);
     }

be7edcac455b062cc384f14f7fe6539141eac9d2
yidao <49662990@qq.com>
2020-08-28 09:09:11 +0800

    添加状态栏测试页面

--- app/src/main/java/com/shuai/test/animation/TestAnimationActivity.java
+++ app/src/main/java/com/shuai/test/animation/TestAnimationActivity.java
@@ -1,15 +1,53 @@
 package com.shuai.test.animation;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.shuai.test.R;
 
-public class TestAnimationActivity extends AppCompatActivity {
+public class TestAnimationActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button mBtnStart;
+    private View mViewAnimation;
+    private AnimatorSet mInAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_animation);
+
+        mBtnStart = findViewById(R.id.btn_start);
+        mViewAnimation = findViewById(R.id.view_animation);
+
+        mBtnStart.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.btn_start) {
+            startInAnimation();
+        }
+    }
+
+    private void startInAnimation() {
+        if (mInAnimation == null) {
+            mInAnimation = new AnimatorSet();
+            ObjectAnimator alphaAnimation = ObjectAnimator.ofFloat(mViewAnimation, "alpha", 0, 1);
+            alphaAnimation.setDuration(300);
+
+            ObjectAnimator scaleXAnimation = ObjectAnimator.ofFloat(mViewAnimation,"scaleX",0,1);
+            scaleXAnimation.setDuration(500);
+
+            ObjectAnimator scaleYAnimation = ObjectAnimator.ofFloat(mViewAnimation,"scaleY",0,1);
+            scaleYAnimation.setDuration(500);
+
+            mInAnimation.playTogether(alphaAnimation,scaleXAnimation,scaleYAnimation);
+
+        }
+        //mInAnimation.start();
     }
 }
---
+++ app/src/main/java/com/shuai/test/statusbar/TestStatusBarActivity.kt
@@ -0,0 +1,13 @@
+package com.shuai.test.statusbar
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.shuai.test.R
+
+class TestStatusBarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_status_bar)
+    }
+}
